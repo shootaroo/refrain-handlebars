50ceb8728e2bc08114f84dac2eda727a50ace583
shootaroo <shotaro.tsubouchi@gmail.com>
2015-01-15 16:44:28 +0900

    Support windows

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ function registerPartial(text, refrain, content) {
   if (match) {
     var isRelative = match[1].indexOf('.') === 0;
     var dir = isRelative ? path.dirname(content.page.filePath) : refrain.options.handlebars.partialsDir;
-    var partial = refrain.load(path.resolve(dir, match[1] + '.hbs'));
+    var file = path.join(path.relative(refrain.options.srcDir, dir), match[1] + '.hbs').replace(/\\/g, '/');
+    var partial = refrain.load(file);
     if (partial) {
       handlebars.registerPartial(match[1], partial.page.template);
     }
